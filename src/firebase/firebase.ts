112970e1469d9b50093ff5213e2c2bcb45afeeaf
jinnai <jinnai.shirakashi@logical-fabrics.com>
2021-09-03 16:39:03 +0900

    テンプレート修正

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_DATABASE,
+  storageBucket: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  appId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_APPID,
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+export const auth = firebase.auth();
+export const db = firebase.firestore();
