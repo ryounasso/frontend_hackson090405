868bcda4416326997bd98f2f8c3778dfc5e565cb
jinnai <jinnai.shirakashi@logical-fabrics.com>
2021-09-04 17:19:12 +0900

    env.localを削除

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APPID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+  apiKey: 'AIzaSyAoo7lpfZZS7DBx1Tqb-Cy50O9otWH-4Ck',
+  authDomain: 'hackathon-fa48c.firebaseapp.com',
+  projectId: 'hackathon-fa48c',
+  storageBucket: 'hackathon-fa48c.appspot.com',
+  messagingSenderId: '699491963854',
+  appId: '1:699491963854:web:9a6bd2d3e9f5ff5b07919f',
+  measurementId: 'G-PQ3GX794L5',
 }
 
 if (!firebase.apps.length) {
